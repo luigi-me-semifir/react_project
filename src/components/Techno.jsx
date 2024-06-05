b85bb2ab445ab41e03525ea2ab54d9059a9858f3
Luigi Me <luigi.menez@semifir.com>
2024-06-05 12:14:15 +0200

    feat: :fire: modification des données

--- src/components/Techno.jsx
+++ src/components/Techno.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import styles from "./Techno.module.scss";
 
 function Techno({ nom, image }) {
+  const [liked, setLiked] = useState(false);
+
+  const handleClick = () => {
+    setLiked(!liked);
+  };
   return (
-    <div className={styles.techno}>
+    <div onClick={handleClick} className={styles.techno}>
       <div className={styles.imageContainer}>
         <img src={image} alt={nom} />
       </div>
       <div
-        className={`${styles.technoTitle} d-flex flex-column justify-content-center align-items-center`}
+        className={`${styles.technoTitle} d-flex flex-row justify-content-around align-items-center`}
       >
         <h3 className="mb-10">{nom}</h3>
+        <i>{liked ? <FaHeart /> : <FaRegHeart />} </i>
       </div>
     </div>
   );
